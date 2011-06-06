e45a1ba8f83c51d5bf5ec5356ef964503c11512b
robertkrimen <robertkrimen@gmail.com>
2011-06-06 16:02:31 -0700

    Re-added basic three20 image gallery

---
+++ 01-three20-gallery/Classes/XyzzyAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  XyzzyAppDelegate.h
+//  Xyzzy
+//
+//  Created by Broken Rim on 3/31/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Three20/Three20.h>
+
+@interface XyzzyAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
