78a0055a7204d47a559b418398fdfca34099e8af
robertkrimen <robertkrimen@gmail.com>
2011-06-26 22:08:43 -0700

    Initial commit for VIMp

---
+++ VIMp/VIMp/VIMp.h
@@ -0,0 +1,15 @@
+//
+//  VIMp.h
+//  VIMp
+//
+//  Created by Broken Rim on 6/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface VIMp : NSObject {
+@private
+}
+
+@end
---
+++ VIMp/VIMp/VIMp.m
@@ -0,0 +1,19 @@
+//
+//  VIMp.m
+//  VIMp
+//
+//  Created by Broken Rim on 6/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "VIMp.h"
+#import <Foundation/Foundation.h>
+
+@implementation VIMp
+
++ (void) load {
+
+   NSLog(@"Hello, World from VIMp");
+}
+
+@end
