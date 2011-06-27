effa9a2b9b3c92170ce22b491838cd5c2719a0b9
robertkrimen <robertkrimen@gmail.com>
2011-06-26 19:26:46 -0700

    Initial commit

---
+++ ImportCSV/ImportCSV/ImportCSVAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  ImportCSVAppDelegate.h
+//  ImportCSV
+//
+//  Created by Broken Rim on 6/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImportCSVAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+@end
